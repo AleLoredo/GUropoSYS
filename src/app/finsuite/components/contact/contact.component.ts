9e0437f3f51ca81302d8484c404574ff976c816a
Yoel Mandelli <yoel.mandelli@techforb.com>
2024-04-21 14:23:36 -0300

    message-alert

--- src/app/finsuite/components/contact/contact.component.ts
+++ src/app/finsuite/components/contact/contact.component.ts
@@ -32,6 +32,7 @@ export class ContactComponent {
   }
 }
  */ import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
@@ -40,6 +41,21 @@ import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  contactForm!: FormGroup;
+  showSuccessMessage: boolean = false;
+  showErrorMessage: boolean = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      message: ['', Validators.required],
+    });
+  }
+
   public sendEmail(e: Event) {
     e.preventDefault();
 
@@ -52,12 +68,16 @@ export class ContactComponent {
       )
       .then(
         (response: EmailJSResponseStatus) => {
-          alert('Mensaje enviado exitosamente!');
-          console.log('Correo electrónico enviado correctamente', response);
-          location.reload(); // Opcional: recargar la página después de enviar el correo electrónico
+          this.showSuccessMessage = true; // Mostrar el mensaje de éxito
+          /* console.log('Correo electrónico enviado correctamente', response); */
+          setTimeout(() => {
+            this.showSuccessMessage = false; // Ocultar el mensaje después de 3 segundos
+          }, 3000);
+          this.contactForm.reset(); // Limpiar el formulario
         },
         (error) => {
-          console.error('Error al enviar correo electrónico', error);
+          this.showErrorMessage = true; // Mostrar el mensaje de error
+          /*     console.error('Error al enviar correo electrónico', error); */
         }
       );
   }
---
+++ src/app/finsuite/services/utilities.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilitiesService {
+  constructor(private _snackBar: MatSnackBar) {}
+
+  mostrarAlertaSuccess(message: string, tipo: string) {
+    this._snackBar.open(message, tipo, {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 5000,
+      panelClass: ['message-success'],
+    });
+  }
+
+  mostrarAlertaError(message: string, tipo: string) {
+    this._snackBar.open(message, tipo, {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 5000,
+      panelClass: ['message-error'],
+    });
+  }
+}
